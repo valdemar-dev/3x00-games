9bb7887bd57127f9596d0d6bfd5776e237cfd66a
v <v@github.noreply.com>
2023-06-03 09:34:38 +0300

    Mobile optimizations

--- app/games/page.js
+++ app/games/page.js
@@ -5,32 +5,32 @@ export default function Games() {
   return (
     <div className={styles.games}>
       <div id={styles.game_list}>
-        <div className={styles.game}>
+        <Link href="/games/blackjack" className={styles.game}>
           <h3>Blackjack</h3>
 
           <p>Aim for 21 without going over.</p>
 
           <span className="unobstructive">Max bet: 20k</span>
-          <Link className="link_primary" href="/games/blackjack">Play</Link>
-        </div>
+          <Link id={styles.play_button} className="link_primary" href="/games/blackjack">Play</Link>
+        </Link>
 
-        <div className={styles.game}>
+        <Link href="/games/coinflip" className={styles.game}>
           <h3>Coinflip</h3>
 
           <p>Call the side before the flip.</p>
 
           <span className="unobstructive">Max bet: 15k</span>
-          <Link className="link_primary" href="/games/coinflip">Play</Link>
-        </div>
+          <Link id={styles.play_button} className="link_primary" href="/games/coinflip">Play</Link>
+        </Link>
 
-        <div className={styles.game}>
+        <Link href="/games/diceroll" className={styles.game}>
           <h3>Dice roll</h3>
 
           <p>Roll an n-sided die.</p>
 
           <span className="unobstructive">Max bet: 10k</span>
-          <Link className="link_primary" href="/games/diceroll">Play</Link>
-        </div>
+          <Link id={styles.play_button} className="link_primary" href="/games/diceroll">Play</Link>
+        </Link>
       </div>
     </div>
   )
